ec6b1fb853875e609734be0036f1d4b7b1dda767
Rigney <krigney@proforma.com>
2018-05-10 13:51:46 -0400

    Got the HTML and JS speaking with angular: Hello, World!

--- public/main.js
+++ public/main.js
@@ -2,4 +2,17 @@ const main = () => {
   document.querySelector('h1').textContent += '?'
 }
 
+angular
+.module("minesweeperApp", [])
+.controller("mainController", ($scope) => {
+
+  $scope.greeting = 'Hello, World!'
+
+});
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', main)
+
