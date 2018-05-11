b7efb2b40897911dfc1bb0fafe74d16fb77ea6fb
Rigney <krigney@proforma.com>
2018-05-11 13:52:22 -0400

    randomly placing 10 mines throughout board. there is a bug where sometimes it will display only 10 but loops 10x.

--- public/main.js
+++ public/main.js
@@ -4,23 +4,24 @@ const main = () => {
 
 const layMinefield = () => {
   var minefield = {};
-    minefield.rows = [];
-    
-    for(var i = 0; i < 9; i++) {
-        var row = {};
-        row.cells = [];
-        
-        for(var j = 0; j < 9; j++) {
-            var cell = {
-              isCovered: true,
-              hasBomb: false,
-              count: 0
-            };            
-            row.cells.push(cell);
-        }
-        
-        minefield.rows.push(row);
+  minefield.rows = [];
+
+  for (var i = 0; i < 9; i++) {
+    var row = {};
+    row.cells = [];
+
+    for (var j = 0; j < 9; j++) {
+      var cell = {
+        // CHANGE IS COVERED BACK TO TRUE!
+        isCovered: false,
+        hasBomb: false,
+        count: 0
+      };
+      row.cells.push(cell);
     }
+
+    minefield.rows.push(row);
+  }
 
   putMine(minefield);
 
@@ -34,10 +35,14 @@ function getCellCoordinates(minefield, row, col) {
 }
 
 function putMine(minefield) {
-  var row = Math.round(Math.random() * 8);
-  var col = Math.round(Math.random() * 8);
-  var cell = getCellCoordinates(minefield, row, col);
-  cell.hasBomb = true;
+  for (var i = 0; i < 10; i++) {
+    var row = Math.round(Math.random() * 8);
+    var col = Math.round(Math.random() * 8);
+    var cell = getCellCoordinates(minefield, row, col);
+    cell.hasBomb = true;
+    //BUG - ON THIRD OR SO REFRESH WILL LOOP 10X BUT WILL ONLY DISPLAY 9 BOMBS
+    console.log(i);
+  }
 }
 
 angular
