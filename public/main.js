3172f3001df64567afda229a5c9f1617fc60fd11
Rigney <krigney@proforma.com>
2018-05-11 22:54:29 -0400

    Tried another approach to 0s. Removes immediatly adjcent cells and doesn't throw error but still won't do it continuously.

--- public/main.js
+++ public/main.js
@@ -61,7 +61,40 @@ const calculate = (minefield, row, col) => {
   }
 }
 
+//APPROACH 1 FOR CLEARING EMPTY CELLS
+
+// const clearEmptyCells = (minefield, row, col) => {
+
+//   for (var newRow = row - 1; newRow <= row + 1; newRow++) {
+
+//     for (var newCol = col - 1; newCol <= col + 1; newCol++) {
+
+//       if (newRow >= 0 && newCol >= 0 && newRow < minefield.rows.length && newCol < minefield.rows[newRow].cells.length) {
+
+//         var cellToCheck = getCellCoordinates(minefield, newRow, newCol);
+
+//         if (cellToCheck.count > 0) {
+//           cellToCheck.isCovered = false;
+//         }
+//         // // THIS WILL AT LEAST CLEAR FIRST ADJACENT CELLS
+//         // if (cellToCheck.count == 0){
+//         //   cellToCheck.isCovered = false;
+//         // }
+//         if (cellToCheck.count == 0){
+//           cellToCheck.isCovered = false;
+//           clearEmptyCells(minefield, newRow, newCol);
+//         }
+//       }
+//     }
+//   }
+// }
+
+
+//APPROACH 2 FOR CLEARING EMPTY CELLS - isn't getting error but still not clearing everything
+
 const clearEmptyCells = (minefield, row, col) => {
+
+  var closeCells = [];
 
   for (var newRow = row - 1; newRow <= row + 1; newRow++) {
 
@@ -70,75 +103,73 @@ const clearEmptyCells = (minefield, row, col) => {
       if (newRow >= 0 && newCol >= 0 && newRow < minefield.rows.length && newCol < minefield.rows[newRow].cells.length) {
 
         var cellToCheck = getCellCoordinates(minefield, newRow, newCol);
-
-        if (cellToCheck.count > 0) {
-          cellToCheck.isCovered = false;
-        }
-        // // THIS WILL AT LEAST CLEAR FIRST ADJACENT CELLS
-        // if (cellToCheck.count == 0){
-        //   cellToCheck.isCovered = false;
-        // }
-        if (cellToCheck.count == 0){
-          cellToCheck.isCovered = false;
-          clearEmptyCells(minefield, newRow, newCol);
-        }
+        closeCells.push(cellToCheck);
       }
     }
+  }
+
+  for (var i = 0; i < closeCells.length; i++)
+  {
+      if (closeCells[i].count == 0 || !closeCells[i].hasBomb)
+      {
+          closeCells[i].isCovered = false;
+          clearEmptyCells(closeCells[i])
+      }
   }
 }
 
 
-  const getNumbers = (minefield) => {
-    for (var i = 0; i < 9; i++) {
-      for (var j = 0; j < 9; j++) {
-        calculate(minefield, i, j);
+const getNumbers = (minefield) => {
+  for (var i = 0; i < 9; i++) {
+    for (var j = 0; j < 9; j++) {
+      calculate(minefield, i, j);
+    }
+  }
+}
+
+const getBombs = (minefield) => {
+  var bombs = [];
+  for (var i = 0; i < 9; i++) {
+    for (var j = 0; j < 9; j++) {
+      var cellToCheck = getCellCoordinates(minefield, i, j);
+      if (cellToCheck.hasBomb) {
+        bombs.push(cellToCheck);
       }
     }
   }
+  return bombs;
+}
 
-  const getBombs = (minefield) => {
-    var bombs = [];
-    for (var i = 0; i < 9; i++) {
-      for (var j = 0; j < 9; j++) {
-        var cellToCheck = getCellCoordinates(minefield, i, j);
-        if (cellToCheck.hasBomb) {
-          bombs.push(cellToCheck);
+angular
+  .module("minesweeperApp", [])
+  .controller("mainController", ($scope) => {
+
+    $scope.gameTitle = 'Minesweeper! in Angular!'
+
+    $scope.minefield = layMinefield();
+
+    $scope.clickCount = 0;
+
+    $scope.showCellContent = (cell, row, col) => {
+      cell.isCovered = false;
+      if (cell.hasBomb) {
+        $scope.playerLost = true;
+        var bombs = getBombs($scope.minefield);
+        for (var i = 0; i < bombs.length; i++) {
+          bombs[i].isCovered = false;
+        }
+      } else {
+        $scope.clickCount++;
+        if (cell.count == 0) {
+          clearEmptyCells($scope.minefield, row, col);
+        }
+        if ($scope.clickCount == 71) {
+          $scope.playerWon = true;
         }
       }
-    }
-    return bombs;
-  }
+    };
 
-  angular
-    .module("minesweeperApp", [])
-    .controller("mainController", ($scope) => {
+  });
 
-      $scope.gameTitle = 'Minesweeper! in Angular!'
-
-      $scope.minefield = layMinefield();
-
-      $scope.clickCount = 0;
-
-      $scope.showCellContent = (cell, row, col) => {
-        cell.isCovered = false;
-        if (cell.hasBomb) {
-          $scope.playerLost = true;
-          var bombs = getBombs($scope.minefield);
-          for (var i = 0; i < bombs.length; i++) {
-            bombs[i].isCovered = false;
-          }
-        } else {
-          $scope.clickCount++;
-          if (cell.count == 0) {
-            clearEmptyCells($scope.minefield, row, col);
-          }
-          if ($scope.clickCount == 71) {
-            $scope.playerWon = true;
-          }
-        }
-      };
-
-    });
-
-  document.addEventListener('DOMContentLoaded', main)
+document.addEventListener('DOMContentLoaded', main)
 
